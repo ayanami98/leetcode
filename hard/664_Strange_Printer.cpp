dd420d2625d215e0c04b54861d7b6c0b6195d8c7
ayanami98 <tufeng2015@foxmail.com>
2017-08-20 04:20:34 -0800

    664

--- hard/664_Strange_Printer.cpp
+++ hard/664_Strange_Printer.cpp
@@ -29,6 +29,13 @@ private:
         dp[l][r] = dfs(s, l, r - 1, dp) + 1; // default value
         for (int i = l; i < r; ++i) {
             if (s[i] == s[r]) {
+                // when trying to complete the printing of interval [i, k] (with S[i] == S[k]),
+                // the job will take the same number of turns as painting [i, k-1]. This is because
+                // it is always at least as good to print [i, k] first in one turn rather than
+                // separately. leave i to the left part instead of right part. In this way, we can 
+                // reduce "the same color" in the left part as well. You can use 
+                // dp[l][r] = std::min(dp[l][r], dfs(s, l, i - 1, dp) + dfs(s, i, r - 1, dp));
+                // but the above judgement state leads to a slower execution.
                 dp[l][r] = std::min(dp[l][r], dfs(s, l, i, dp) + dfs(s, i + 1, r - 1, dp));
             }
         }
